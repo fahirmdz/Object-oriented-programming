6df54432b844657655eca9303254cd308551059a
fahirmdz <fahir@live.com>
2019-01-10 16:12:56 +0100

    Finished Workshop 12 - Rjesenje

--- Workshop 12/Workshop 12 - Rjesenje.cpp
+++ Workshop 12/Workshop 12 - Rjesenje.cpp
@@ -1,3 +1,6 @@
+//fahirmdz GitHub
+
+
 #include"pch.h"
 #include<iostream>
 #include<vector>
@@ -7,6 +10,8 @@
 #include<exception>
 #include<algorithm>
 #include<iterator>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
 char * AlocirajNizKaraktera(const char * sadrzaj) {
@@ -51,7 +56,7 @@ Term(const char *key,const char* value){
 	if (key == nullptr || value == nullptr || strlen(value) < 2)
 		throw Error("Parametri za pojam nisu validni!", __DATE__, __TIME__, __FUNCTION__, __LINE__);
 	_key = AlocirajNizKaraktera(key);
-	_value = AlocirajNizKaraktera(_value);
+	_value = AlocirajNizKaraktera(value);
 }
 Term(const Term& t):_value(AlocirajNizKaraktera(t._value)),_key(AlocirajNizKaraktera(t._key)){}
 ~Term() {
@@ -59,7 +64,7 @@ Term(const Term& t):_value(AlocirajNizKaraktera(t._value)),_key(AlocirajNizKarak
 	delete[] _value; _value = nullptr;
 }
 
-Term& operator=(Term& t) {
+Term& operator=(const Term& t) {
 	if (this == &t)
 		return *this;
 
@@ -107,9 +112,9 @@ class Dictionary {
      vector<Term> _terms; 
 
 public: 
-Dictionary():_title(""),_publicationYear(2019){}
-Dictionary(string title,int year){
-	if (title.compare("") || year <= 1950 || year > 2019)
+Dictionary():_title("-"),_publicationYear(2019){}
+Dictionary(const char* title,int year){
+	if (title==nullptr  || strlen(title)<2 || year <= 1950 || year > 2019)
 		throw Error("Parametri za rijecnik nisu validni!", __DATE__, __TIME__, __FUNCTION__, __LINE__);
 	_title = title;
 	_publicationYear = year;
@@ -132,7 +137,7 @@ Dictionary& operator=(Dictionary& d) {
 //Sprijeciti dodavanje istih vrijednosti u kolekciju (baciti izuzetak). 
 //Pojmovi u svakom momentu treba da budu sortirani abecedno 
 //(koristiti funkciju sort iz biblioteke algorithm).
-Dictionary& operator()(char* key, const char* value) {
+Dictionary& operator()(const char* key, const char* value) {
 	try {
 		vector<Term>::iterator it;
 		Term* temp = new Term(key, value);
@@ -140,7 +145,7 @@ Dictionary& operator()(char* key, const char* value) {
 			if (*it == *temp)
 				throw Error("Pojam vec postoji u rijecniku! Ne mogu se dodati 2 ista pojma!", __DATE__, __TIME__, __FUNCTION__, __LINE__);
 		_terms.push_back(*temp);
-		sort(_terms.begin(), _terms.end());
+		sort(_terms.begin(), _terms.end());  //SORTIRA PO ABECEDNOM REDOSLIJEDU
 	}
 	catch (Error& obj) {
 		obj.Info();
@@ -152,10 +157,11 @@ Dictionary& operator()(char* key, const char* value) {
 //Operator -= za uklanjanje pojma iz rijecnika na osnovu atributa _key.
 Dictionary& operator-=(const char* key) {
 	vector<Term>::iterator it;
-	for (it=_terms.begin();it!=_terms.end();it++)
+	for (it = _terms.begin(); it != _terms.end(); it++) {
 		if (strcmp(it->GetKey(), key) == 0) {
 			_terms.erase(it);
 		}
+	}
 	return *this;
 }
 
@@ -171,18 +177,34 @@ char* operator[](const char* key) {
 			return it->GetValue();
 	return nullptr;
 }
-
+Term& operator[](int index) {
+	if (index<0 || index>(int)_terms.size() - 1)
+		throw Error("Index nije validan!", __DATE__, __TIME__, __FUNCTION__, __LINE__);
+	return _terms[index];
+}
 //Funkciju koja vraca kolekciju kljuceva (bosanskih rijeci) generisanih slucajnim odabirom.
 //Generisanje slucajnog broja izmedu min i max: int randNum = rand()%(max-min + 1) + min; 
 //Funkcija kao parametar prima broj rijeci koje je potrebno generisati.
 vector<string> returnKey(int brk) {
-	if (brk <= 0 || (unsigned int)brk>_terms.size())
+	if ((unsigned int)brk>_terms.size())
 		return vector<string>();
 
 	vector<string> keys;
-
-	for (int i = 0; i < brk; i++) {
-		keys.push_back(_terms[rand() % _terms.size()].GetKey());
+	int i = 0;
+	while (i < brk) {
+		srand(time(0));
+		int randkey = rand() % (int)_terms.size();
+		int finalrand = randkey;
+		
+		int flag = 0;
+		for(int j=0;j<(int)keys.size();j++)
+			if (strcmp(keys[j].c_str(), _terms[finalrand].GetKey()) == 0) {
+				flag = 1;
+			}
+		if (flag == 0) {
+			keys.push_back(_terms[finalrand].GetKey());
+			i++;
+		}
 	}
 	return keys;
 }
@@ -218,7 +240,9 @@ public:
 			throw Error("Parametri za takmicara nisu validni!", __DATE__, __TIME__, __FUNCTION__, __LINE__);
 		_ime = ime;
 		_prezime = prezime;
-		_pitanja = _rijecnik.returnKey(10);
+		_pitanja = _rijecnik.returnKey(5);
+		for (int i = 0; i < (int)_pitanja.size(); i++)
+			_odgovori.push_back(new string(""));
 	}
 	Competitor(const Competitor& cm) :_ime(cm._ime), _prezime(cm._prezime) {
 		_pitanja = cm._pitanja;
@@ -240,9 +264,8 @@ public:
 		_ime = c._ime;
 		_prezime = c._prezime;
 		_pitanja = c._pitanja;
-		for (unsigned int i = 0; i < _odgovori.size(); i++)
-			_odgovori.push_back(new string(*_odgovori[i]));
-		_pitanja = c._pitanja;
+		for (unsigned int i = 0; i < c._odgovori.size(); i++)
+			_odgovori.push_back(new string(*c._odgovori[i]));
 		_rijecnik = c._rijecnik;
 		return *this;
 	}
@@ -255,41 +278,163 @@ public:
 		return *this;
 	}
 
-	void DodajOdgovor(string odg) {
-		_odgovori.push_back(new string(odg));
+	void DodajOdgovor(int rbOdg,const char* odg) { 
+		if (rbOdg<0 || rbOdg>(int)_pitanja.size()-1)
+			return;
+		vector<string*>::iterator it = _odgovori.begin() + rbOdg;
+		*it = new string(odg);
 	}
+
 	int brojOsvojenihBodova() {
-		if (_odgovori.size() != _pitanja.size() || _odgovori.size()==0) {
+		if (_odgovori.size() != _pitanja.size() || _odgovori.size() == 0) {
 			return 0;
 		}
 		int uk = 0;
-		for (unsigned int i = 0; i < _odgovori.size(); i++)
-			for (unsigned int j = 0; j < _pitanja.size(); j++)
-				if (_pitanja[j].compare(*_odgovori[i]))
-					uk += 1;
+		for (unsigned int i = 0; i < _odgovori.size(); i++) {
+			if (_odgovori[i]->compare(_rijecnik[_pitanja[i].c_str()]) == 0)
+				uk += 1;
+		}
 
 		return uk;
+	}
+	void printPitanja() {
+		cout << "--PITANJA--\n---------------------------------\n";
+		for (unsigned int i = 0; i < _pitanja.size(); i++)
+			cout << i + 1 << ". " << _pitanja[i] << endl;
+	}
+	void pokreniTest() {
+		cout << "\n----TEST ENGLESKOG JEZIKA----\n------------------------------\n";
+		int i = 0;
+
+
+		auto inputOdgovor = [=](int i) {
+			char* x = new char[50];
+			cout << i + 1 << ". odgovor ---> ";
+			cin.getline(x, 50);
+			if (strcmp(x, "0") != 0)
+				DodajOdgovor(i, x);
+			else
+				return false;
+			return true;
+		};
+		printPitanja();
+		cout << endl << endl;
+		bool flag = true;
+		do {
+			flag = inputOdgovor(i++);
+		} while (i != 5 && flag);
+
+		char vrati = 'N', ispraviJos = 'D ';
+
+		do {
+			cout << "\nDa li zelite da ispravite neki odgovor?(D/N) ";
+			cin >> vrati;
+
+			if (toupper(vrati) == 'D') {
+				int rb;
+					cout << "-Unesite Rb. odgovora i novi odgovor-\nRedni broj: ";
+					do {
+						cin >> rb;
+						if (rb < 1 || rb>5)
+							cout << "Pogresan redni broj! Pokusajte opet..(0-IZLAZ)\n";
+					} while (rb < 1 || rb>5);
+					cin.ignore();
+					inputOdgovor(rb - 1);
+
+				do {
+					cout << "Ispravi jos odgovora?(D/N) ";
+					cin >> ispraviJos;
+				} while (toupper(ispraviJos) != 'N' && toupper(ispraviJos) != 'D');
+			}
+
+
+		} while ((toupper(vrati)!='D' && toupper(vrati)!='N') || toupper(ispraviJos)=='D');
+
+	}
+
+	string GetImePrezime() { return string(_ime + " " + _prezime); }
+	void Info() {
+		cout << "\n-------TAKMICAR INFO---------\n----------------------------------\n";
+		cout << "Ime: " << _ime << "\nPrezime: " << _prezime << endl;
+		printPitanja();
+		cout << "\n--ODGOVORI--\n--------------------------------\n";
+		for (unsigned int i = 0; i < _odgovori.size(); i++)
+			cout << i + 1 << ". " << *_odgovori[i] << endl;
 	}
 };
 
 
-int main() { 
-	
-	//Kreirati rijecnik pojmova. 
-	//Kreirati niz objekata tipa Competitor i simulirati takmicenje u poznavanju rijeci engleskog jezika. 
-	//Pronaci najboljeg medu takmicarima (najveci broj bodova).
-	Term* _pojmovi[10];
-	_pojmovi[0] = new Term("Stolica", "Chair");
-	_pojmovi[1] = new Term("Crna", "Black");
-	_pojmovi[2] = new Term("Zabava", "Entertainment");
-	_pojmovi[3] = new Term("Zvijer", "Beast");
-	_pojmovi[4] = new Term("Polimorfizam", "Polimorphysm");
-	_pojmovi[5] = new Term("Suncane naocare", "Sunglasses");
-	_pojmovi[6] = new Term("Voda", "Water");
-	_pojmovi[7] = new Term("Pozuri!", "Hurry up!");
-	_pojmovi[8] = new Term("Voz", "Train");
-	_pojmovi[9] = new Term("Sjediste", "Seat");
+int main() {
 
+	try {
+		//Kreirati rijecnik pojmova. 
+		//Kreirati niz objekata tipa Competitor i simulirati takmicenje u poznavanju rijeci engleskog jezika. 
+		//Pronaci najboljeg medu takmicarima (najveci broj bodova).
+		Term* _pojmovi[10];
+		_pojmovi[0] = new Term("Stolica", "Chair");
+		_pojmovi[1] = new Term("Crna", "Black");
+		_pojmovi[2] = new Term("Zabava", "Entertainment");
+		_pojmovi[3] = new Term("Zvijer", "Beast");
+		_pojmovi[4] = new Term("Polimorfizam", "Polimorphysm");
+		_pojmovi[5] = new Term("Suncane naocare", "Sunglasses");
+		_pojmovi[6] = new Term("Voda", "Water");
+		_pojmovi[7] = new Term("Pozuri!", "Hurry up!");
+		_pojmovi[8] = new Term("Voz", "Train");
+		_pojmovi[9] = new Term("Sjediste", "Seat");
+
+		Dictionary _rijecnik("Bosansko-Engleski rijecnik", 2007);
+		_rijecnik("Stolica", "Chair");
+		_rijecnik("Crna", "Black");
+		_rijecnik("Zabava", "Entertainment");
+		_rijecnik("Zvijer", "Beast");
+		_rijecnik("Polimorfizam", "Polimorphysm");
+		_rijecnik("Suncane naocare", "Sunglasses");
+		_rijecnik("Voda", "Water");
+		_rijecnik("Voz", "Train");
+		_rijecnik("Sjediste", "Seat");
+
+		cout << "\n============TESTIRANJE klase Dictionary==============\n\n";
+		_rijecnik.Info();
+
+
+		cout << "\n------Testiranje konstruktora kopije-------\n";
+		Dictionary _rijecnik2(_rijecnik);
+		cout << "\n---RIJECNIK 2-----\n";
+		_rijecnik2.Info();
+
+		cout << "\n------Testiranje operatora dodijele-------\n";
+		Dictionary _rijecnik3;
+		_rijecnik3 = _rijecnik2;
+		cout << "\n---RIJECNIK 3-----\n";
+		_rijecnik3.Info();
+
+		cin.get();
+		system("cls");
+		cout << "\n================================================\n============TESTIRANJE klase Dictionary==============\n\n";
+
+		Competitor c1("John", "Newman", _rijecnik);
+		c1.Info();
+
+		cout << "\n------Testiranje konstruktora kopije-------\n";
+		cout << "\nTAKMICAR C2\n";
+		Competitor c2(c1);
+		c2.Info();
+
+		cout << "\n------Testiranje operatora dodijele-------\n";
+		cout << "\nTAKMICAR C3\n";
+		Competitor c3;
+		c3=c2;
+		c3.Info();
+		cin.get();
+		system("cls");
+
+		c1.pokreniTest();
+		c1.Info();
+		cout << "\n\nTakmicar " << c1.GetImePrezime() << " je osvojio " << c1.brojOsvojenihBodova() << " bodova na testu\n\n";
+	}
+	catch (Error& obj) {
+		obj.Info();
+	}
 
 
 
